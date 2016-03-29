0b07ef458532db550a75c4245aa7cdc9994b8895
Norman <el.norman01@gmail.com>
2016-03-28 22:20:07 -0400

    Vista de divorcio agregada

---
+++ AbogadosEasySoft/Views/Frm_Divorcios.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace AbogadosEasySoft.Views
+{
+    partial class Frm_Divorcios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtDivorcioId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDemandanteNombre = new System.Windows.Forms.TextBox();
+            this.txtDemandanteSegundoNombre = new System.Windows.Forms.TextBox();
+            this.txtDemandanteApellido = new System.Windows.Forms.TextBox();
+            this.txtDemandanteSegundoApellido = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtDemandadoSegundoApellido = new System.Windows.Forms.TextBox();
+            this.txtDemandadoApellido = new System.Windows.Forms.TextBox();
+            this.txtDemandadoSegundoNombre = new System.Windows.Forms.TextBox();
+            this.txtDemandadoNombre = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DIVORCIOS";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtDemandanteSegundoApellido);
+            this.groupBox1.Controls.Add(this.txtDemandanteApellido);
+            this.groupBox1.Controls.Add(this.txtDemandanteSegundoNombre);
+            this.groupBox1.Controls.Add(this.txtDemandanteNombre);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 149);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(214, 140);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Demandante";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Segundo Apellido";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Segundo Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Primer Nombre";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtDivorcioId);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(12, 44);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 41);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Búsqueda Divorcio";
+            // 
+            // txtDivorcioId
+            // 
+            this.txtDivorcioId.Location = new System.Drawing.Point(78, 13);
+            this.txtDivorcioId.Name = "txtDivorcioId";
+            this.txtDivorcioId.Size = new System.Drawing.Size(116, 20);
+            this.txtDivorcioId.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "No. Divorcio";
+            // 
+            // txtDemandanteNombre
+            // 
+            this.txtDemandanteNombre.Location = new System.Drawing.Point(102, 16);
+            this.txtDemandanteNombre.Name = "txtDemandanteNombre";
+            this.txtDemandanteNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtDemandanteNombre.TabIndex = 4;
+            // 
+            // txtDemandanteSegundoNombre
+            // 
+            this.txtDemandanteSegundoNombre.Location = new System.Drawing.Point(102, 42);
+            this.txtDemandanteSegundoNombre.Name = "txtDemandanteSegundoNombre";
+            this.txtDemandanteSegundoNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtDemandanteSegundoNombre.TabIndex = 5;
+            // 
+            // txtDemandanteApellido
+            // 
+            this.txtDemandanteApellido.Location = new System.Drawing.Point(102, 68);
+            this.txtDemandanteApellido.Name = "txtDemandanteApellido";
+            this.txtDemandanteApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtDemandanteApellido.TabIndex = 6;
+            // 
+            // txtDemandanteSegundoApellido
+            // 
+            this.txtDemandanteSegundoApellido.Location = new System.Drawing.Point(102, 94);
+            this.txtDemandanteSegundoApellido.Name = "txtDemandanteSegundoApellido";
+            this.txtDemandanteSegundoApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtDemandanteSegundoApellido.TabIndex = 7;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtDemandadoSegundoApellido);
+            this.groupBox2.Controls.Add(this.txtDemandadoApellido);
+            this.groupBox2.Controls.Add(this.txtDemandadoSegundoNombre);
+            this.groupBox2.Controls.Add(this.txtDemandadoNombre);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Location = new System.Drawing.Point(232, 149);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(214, 140);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Demandante";
+            // 
+            // txtDemandadoSegundoApellido
+            // 
+            this.txtDemandadoSegundoApellido.Location = new System.Drawing.Point(102, 94);
+            this.txtDemandadoSegundoApellido.Name = "txtDemandadoSegundoApellido";
+            this.txtDemandadoSegundoApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtDemandadoSegundoApellido.TabIndex = 7;
+            // 
+            // txtDemandadoApellido
+            // 
+            this.txtDemandadoApellido.Location = new System.Drawing.Point(102, 68);
+            this.txtDemandadoApellido.Name = "txtDemandadoApellido";
+            this.txtDemandadoApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtDemandadoApellido.TabIndex = 6;
+            // 
+            // txtDemandadoSegundoNombre
+            // 
+            this.txtDemandadoSegundoNombre.Location = new System.Drawing.Point(102, 42);
+            this.txtDemandadoSegundoNombre.Name = "txtDemandadoSegundoNombre";
+            this.txtDemandadoSegundoNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtDemandadoSegundoNombre.TabIndex = 5;
+            // 
+            // txtDemandadoNombre
+            // 
+            this.txtDemandadoNombre.Location = new System.Drawing.Point(102, 16);
+            this.txtDemandadoNombre.Name = "txtDemandadoNombre";
+            this.txtDemandadoNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtDemandadoNombre.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 97);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Segundo Apellido";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Apellido";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 45);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(90, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Segundo Nombre";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 16);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(76, 13);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Primer Nombre";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(18, 321);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(29, 13);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "Juez";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(80, 318);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 10;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(18, 360);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(58, 13);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Honorarios";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(80, 357);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 12;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(151, 401);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 13;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(232, 401);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 14;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(229, 321);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(85, 13);
+            this.label13.TabIndex = 15;
+            this.label13.Text = "Tipo de Divorcio";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(320, 318);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 16;
+            // 
+            // Frm_Divorcios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 436);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Frm_Divorcios";
+            this.Text = "Frm_Divorcios";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtDivorcioId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDemandanteSegundoApellido;
+        private System.Windows.Forms.TextBox txtDemandanteApellido;
+        private System.Windows.Forms.TextBox txtDemandanteSegundoNombre;
+        private System.Windows.Forms.TextBox txtDemandanteNombre;
+        private System.Windows.Forms.TextBox txtDemandadoSegundoApellido;
+        private System.Windows.Forms.TextBox txtDemandadoApellido;
+        private System.Windows.Forms.TextBox txtDemandadoSegundoNombre;
+        private System.Windows.Forms.TextBox txtDemandadoNombre;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
